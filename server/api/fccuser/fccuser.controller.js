6b5bd415c4f11ac7c8c71f55071dd986885882ff
roelver <roel@romaniflo.nl>
2016-04-26 22:43:26 +0200

    Loadmore skip more users on start

--- server/api/fccuser/fccuser.controller.js
+++ server/api/fccuser/fccuser.controller.js
@@ -258,7 +258,7 @@ exports.loadMore = function(req, res) {
 
    Fccuser.find().count().exec(function (err, count) {
       if (err) return handleError(res, err);
-      setTimeout(loadNextChunk, 0, count+1000, 5);
+      setTimeout(loadNextChunk, 0, count+2400, 5);
 
       return res.status(200).send('<h1>Loading new users from chat starting from '+count+'. Keep an eye on the logs.</h1>');
     });
